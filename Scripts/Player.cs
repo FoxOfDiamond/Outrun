78e174a632d837fb8a6f631d47a034f983a011fd
FoxOfDiamond <9tales9faces@gmail.com>
2025-08-02 13:36:08 +0700

    Split

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,153 +4,38 @@ using Godot.Collections;
 
 namespace Outrun;
 
+[GlobalClass]
 public partial class Player : CharacterBody3D
 {
-	public Camera3D Camera;
-	private Vector3 CameraOffset = new(0,4,0);
-	private Vector3 CameraRotationalOffset = new(-20,0,0);
-	private bool mouseLocked = true;
-	private bool freelook = false;
-	private float zoom = 10;
-	private bool drifting = false;
-	private float baseSpeed = 0.7f;
-	private float baseTurnSpeed = 0.3f;
-	private float speed;
-	private float turnSpeed;
-	private Vector3 AngularVelocity = new();
-	private Vector3 gravity;
-	private readonly Dictionary<Key, bool> Inputs = new Dictionary<Key, bool>()
-	{
-		[Key.W] = false,
-		[Key.A] = false,
-		[Key.S] = false,
-		[Key.D] = false,
-		[Key.E] = false,
-		[Key.Q] = false,
-		[Key.C] = false,
-		[Key.Space] = false
-	};
-
+	[Export]
+	Camera3D camera { get; set;}
+	[Export]
+	Vector3 cameraOffset { get; set; } = new(0, 4, 0);
+	[Export]
+	float cameraZoom { get; set; } = 10;
+	[Export]
+	float gravityStrength { get; set; } = 9.8f;
+	[Export]
+	float speed { get; set; } = 0.7f;
+	[Export]
+	float turnCircumference { get; set; } = 20f;
+	private PlayerController controller;
 	public override void _Ready()
 	{
-		Camera = GetNode<Camera3D>("../Camera");
-		Input.MouseMode = Input.MouseModeEnum.Captured;
-		gravity = (float)ProjectSettings.GetSetting("physics/3d/default_gravity") * (Vector3)ProjectSettings.GetSetting("physics/3d/default_gravity_vector");
-		speed = baseSpeed;
-		turnSpeed = baseTurnSpeed;
-
+		controller = new(this, camera, cameraOffset, cameraZoom, gravityStrength, speed, turnCircumference);
+		controller._Ready();
 	}
 	public override void _Process(double delta)
 	{
-		if (mouseLocked && !freelook)
-		{
-			if (drifting)
-			{
-				Camera.RotationDegrees = RotationDegrees + CameraRotationalOffset + new Vector3(0, AngularVelocity.Y > 0 ? -15 : 15, 0);
-
-			}
-			else
-			{
-				Camera.RotationDegrees = RotationDegrees + CameraRotationalOffset;
-			}
-		}
-		Camera.Position = Position + Camera.GlobalBasis.Z * zoom + Camera.GlobalBasis.Z * CameraOffset.Z + Camera.GlobalBasis.X * CameraOffset.X + Camera.GlobalBasis.Y * CameraOffset.Y;
+		controller._Process(delta);
 	}
 	public override void _PhysicsProcess(double delta)
 	{
-		if (Inputs[Key.W])
-		{
-			Velocity -= GlobalBasis.Z * speed;
-		}
-		if (Inputs[Key.S])
-		{
-			Velocity += GlobalBasis.Z * speed;
-		}
-		if (drifting)
-		{
-			if (Mathf.Abs(AngularVelocity.Y) < 3)
-			{
-				drifting = false;
-			}
-			turnSpeed = baseTurnSpeed * 0.7f;
-		}
-		else
-		{
-			turnSpeed = baseTurnSpeed;
-		}
-		if (Inputs[Key.A])
-		{
-			AngularVelocity += new Vector3(0, turnSpeed, 0);
-		}
-		if (Inputs[Key.D])
-		{
-			AngularVelocity -= new Vector3(0,turnSpeed,0);
-		}
-		RotationDegrees += AngularVelocity;
-		AngularVelocity *= 0.9f;
-		Velocity += gravity * 0.1f;
-		Velocity *= 0.95f;
-		MoveAndSlide();
+		controller._PhysicsProcess(delta);
 	}
 	public override void _Input(InputEvent @event)
 	{
-		base._Input(@event);
-		if (@event is InputEventKey keyEvent)
-		{
-			if (Inputs.ContainsKey(keyEvent.Keycode))
-			{
-				Inputs[keyEvent.Keycode] = keyEvent.Pressed;
-			}
-			if (keyEvent.Keycode == Key.Escape && keyEvent.Pressed)
-			{
-				mouseLocked = !mouseLocked;
-				if (mouseLocked)
-				{
-					Input.MouseMode = Input.MouseModeEnum.Captured;
-				}
-				else
-				{
-					Input.MouseMode = Input.MouseModeEnum.Visible;
-				}
-			}
-			if (keyEvent.Keycode == Key.Space)
-			{
-				if (keyEvent.Pressed && (IsOnFloor() || drifting))
-				{
-					if (!drifting)
-					{  
-						Velocity += Vector3.Up * 15;
-					}
-					drifting = true;
-				}
-				else
-				{ 
-					drifting = false;
-				}
-			}
-		}
-		if (@event is InputEventMouseButton mouseButtonEvent)
-		{
-			if (mouseButtonEvent.ButtonIndex == MouseButton.Right)
-			{
-				freelook = mouseButtonEvent.Pressed;
-			}
-			if (mouseButtonEvent.ButtonIndex == MouseButton.WheelUp)
-			{
-				zoom *= 0.9f;
-			}
-			if (mouseButtonEvent.ButtonIndex == MouseButton.WheelDown)
-			{
-				zoom *= 1.1f;
-			}
-		}
-		if (@event is InputEventMouseMotion mouseEvent)
-		{
-			if (freelook)
-			{
-				Camera.RotationDegrees += new Vector3(mouseEvent.ScreenRelative.Y * -0.1f, mouseEvent.ScreenRelative.X * -0.1f, 0);
-			}
-		}
+		controller._Input(@event);
 	}
 
 }
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,166 @@
+using Godot;
+using Godot.Collections;
+
+namespace Outrun;
+
+public class PlayerController
+{
+    public Player player;
+	public Camera3D camera;
+	private Vector3 cameraOffset = new(0,4,0);
+	private Vector3 cameraRotationalOffset = new(-20,0,0);
+	private bool mouseLocked = true;
+	private bool freelook = false;
+	private float cameraZoom = 10;
+	private bool drifting = false;
+	private float baseSpeed = 0.7f;
+	private float speed;
+	private float baseTurnCircumference = 20;
+	private float turnCircumference = 20;
+	private float turnSpeed;
+	private Vector3 angularVelocity = new();
+	private Vector3 gravity;
+	private float gravityStrength;
+	private readonly Dictionary<Key, bool> Inputs = new()
+	{
+		[Key.W] = false,
+		[Key.A] = false,
+		[Key.S] = false,
+		[Key.D] = false,
+		[Key.E] = false,
+		[Key.Q] = false,
+		[Key.C] = false,
+		[Key.Space] = false
+	};
+    public PlayerController(Player _player, Camera3D _camera, Vector3 _cameraOffset, float _cameraZoom, float _gravityStrength, float _speed, float _turnCircumference)
+    {
+        player = _player;
+        camera = _camera;
+        cameraOffset = _cameraOffset;
+        cameraZoom = _cameraZoom;
+        gravityStrength = _gravityStrength;
+        baseSpeed = _speed;
+        baseTurnCircumference = _turnCircumference;
+    }
+
+    public void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+        gravity = gravityStrength * (Vector3)ProjectSettings.GetSetting("physics/3d/default_gravity_vector");
+        speed = baseSpeed;
+        turnCircumference = baseTurnCircumference;
+
+    }
+	public void _Process(double delta)
+	{
+		if (mouseLocked && !freelook)
+		{
+			if (drifting)
+			{
+				camera.RotationDegrees = player.RotationDegrees + cameraRotationalOffset + new Vector3(0, angularVelocity.Y > 0 ? -15 : 15, 0);
+
+			}
+			else
+			{
+				camera.RotationDegrees = player.RotationDegrees + cameraRotationalOffset;
+			}
+		}
+		camera.Position = player.Position + camera.GlobalBasis.Z * cameraZoom + camera.GlobalBasis.Z * cameraOffset.Z + camera.GlobalBasis.X * cameraOffset.X + camera.GlobalBasis.Y * cameraOffset.Y;
+	}
+	public void _PhysicsProcess(double delta)
+	{
+		if (Inputs[Key.W])
+		{
+			player.Velocity -= player.GlobalBasis.Z * speed;
+		}
+		if (Inputs[Key.S])
+		{
+			player.Velocity += player.GlobalBasis.Z * speed;
+		}
+        if (drifting)
+        {
+            if (Mathf.Abs(angularVelocity.Y) < 3)
+            {
+                drifting = false;
+            }
+            turnCircumference = baseTurnCircumference * 0.7f;
+        }
+        else
+        {
+            turnCircumference = baseTurnCircumference;
+		}
+		turnSpeed = player.Velocity.Length() / (turnCircumference / 360);
+		if (Inputs[Key.A])
+		{
+			angularVelocity = new Vector3(0, turnSpeed, 0);
+		}
+		if (Inputs[Key.D])
+		{
+			angularVelocity = new Vector3(0,-turnSpeed,0);
+		}
+		player.RotationDegrees += angularVelocity;
+		angularVelocity *= 0.9f;
+		player.Velocity += gravity * 0.1f;
+		player.Velocity *= 0.95f;
+		player.MoveAndSlide();
+	}
+	public void _Input(InputEvent @event)
+	{
+		if (@event is InputEventKey keyEvent)
+		{
+			if (Inputs.ContainsKey(keyEvent.Keycode))
+			{
+				Inputs[keyEvent.Keycode] = keyEvent.Pressed;
+			}
+			if (keyEvent.Keycode == Key.Escape && keyEvent.Pressed)
+			{
+				mouseLocked = !mouseLocked;
+				if (mouseLocked)
+				{
+					Input.MouseMode = Input.MouseModeEnum.Captured;
+				}
+				else
+				{
+					Input.MouseMode = Input.MouseModeEnum.Visible;
+				}
+			}
+			if (keyEvent.Keycode == Key.Space)
+			{
+				if (keyEvent.Pressed && (player.IsOnFloor() || drifting))
+				{
+					if (!drifting)
+					{  
+						player.Velocity += Vector3.Up * 15;
+					}
+					drifting = true;
+				}
+				else
+				{ 
+					drifting = false;
+				}
+			}
+		}
+		if (@event is InputEventMouseButton mouseButtonEvent)
+		{
+			if (mouseButtonEvent.ButtonIndex == MouseButton.Right)
+			{
+				freelook = mouseButtonEvent.Pressed;
+			}
+			if (mouseButtonEvent.ButtonIndex == MouseButton.WheelUp)
+			{
+				cameraZoom *= 0.9f;
+			}
+			if (mouseButtonEvent.ButtonIndex == MouseButton.WheelDown)
+			{
+				cameraZoom *= 1.1f;
+			}
+		}
+		if (@event is InputEventMouseMotion mouseEvent)
+		{
+			if (freelook)
+			{
+				camera.RotationDegrees += new Vector3(mouseEvent.ScreenRelative.Y * -0.1f, mouseEvent.ScreenRelative.X * -0.1f, 0);
+			}
+		}
+	}
+}
